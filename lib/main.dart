bcc7d4f53fc1c1ac3d3164a93bf1a5aa2552e88b
dhrubrawat66 <sabitradhandhrubrawat66@gmail.com>
2022-02-15 14:57:15 +0545

    step-4

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import "dart:developer" as devtools show log;
-import 'dart:io';
-import 'package:meta/meta.dart';
-import 'dart:convert';
 
 extension Log on Object {
   void log() => devtools.log(toString());
 }
 
-extension GetOnUri on Object {
-  Future<HttpClientResponse> getUrl(
-    String url,
-  ) =>
-      HttpClient().getUrl(Uri.parse(url)).then(
-            (req) => req.close(),
-          );
+abstract class CanRun {
+  void run();
 }
 
-mixin CanMakeGetCall {
-  String get url;
-  @useResult
-  Future<String> getString() => getUrl(
-        url,
-      ).then(
-        (resp) => resp
-            .transform(
-              utf8.decoder,
-            )
-            .join(),
-      );
-}
-
-@immutable
-class GetPeople with CanMakeGetCall {
-  const GetPeople();
+class Cat extends CanRun {
   @override
-  String get url => 'http://127.0.0.1:5500/api/people.json';
+  void run() {}
 }
 
-void testIt() async {
-  final people = await const GetPeople().getString();
-  people.log();
-}
+void testIt() async {}
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
